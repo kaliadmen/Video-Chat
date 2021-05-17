254279c1fbb01d0700a2a51078c67b00387049f3
kaliadmen <kaliadmen@gmail.com>
2021-05-17 12:21:25 -0400

    ADDS SOCKET HANDLERS - added socket handlers for application functions

--- index.js
+++ index.js
@@ -16,4 +16,20 @@ app.get("/", (req, res) => {
     res.send("Server is running.")
 });
 
+io.on('connection', (socket) => {
+    socket.emit('me',socket.id);
+
+    socket.on('call_user', ({ userToCall, signalData, from, name}) => {
+        io.to(userToCall).emit('call_user', {signal: signalData, from, name});
+    });
+
+    socket.on('call_answered', (data) => {
+        io.to(data.to).emit('call_accepted', data.signal);
+    })
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('call_ended');
+    })
+});
+
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
